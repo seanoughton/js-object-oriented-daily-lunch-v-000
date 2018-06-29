6efc0ce439afae339032ad5d4095ee8a7505475b
Sean Oughton <seanoughton@gmail.com>
2018-06-29 17:06:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ class Neighborhood {
     function onlyUnique(value, index, self) {
       return self.indexOf(value) === index;
     }
-    return thisNeighborhoodDeliveries.filter( onlyUnique );
+    return thisNeighborhoodDeliveries.unique();
   };
 
 }
