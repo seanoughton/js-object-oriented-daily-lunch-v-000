2201b1c9e02c87f74284eeb4c3c58d9a3ed3e6c6
Sean Oughton <seanoughton@gmail.com>
2018-06-29 17:28:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ class Neighborhood {
       //go through each element in the array and compare it to the previous one
       //if the two elements are the same, then don't add them to the new array
       const idSorter = function (object1,object2) {
-        if (object1.id == object2.id) {
+        if (object1.id === object2.id) {
           return 
         } else {
           return object1.id - object2.id
